712b93b6ecedd4da7a8d1184a16cea8571254c63
maxime.kerlidou <mkerlidou@goweb.fr>
2023-02-22 15:24:25 +0100

    header integration

---
+++ src/components/Header/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <header class="header">
+    <!-- contenu du composant -->
+  </header>
+</template>
+
+<script lang="ts">
+import "./Header.style.scss";
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Header",
+});
+</script>
+
+<style scoped lang="scss">
+/* styles du composant */
+</style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import sass from "sass";
+import { resolve } from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  css: {
+    preprocessorOptions: {
+      sass: {
+        implementation: sass,
+        additionalData: `@import "@/assets/scss/_variables.scss";`,
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "src"),
+    },
+  },
+});
