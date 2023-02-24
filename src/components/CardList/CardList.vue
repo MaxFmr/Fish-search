43afc6ed25edc9100af795aca8a6c043bdd24a04
maxime.kerlidou <mkerlidou@goweb.fr>
2023-02-24 10:43:23 +0100

    search bar good

--- src/components/CardList/CardList.vue
+++ src/components/CardList/CardList.vue
@@ -1,15 +1,19 @@
 <template>
   <div class="container">
-    <div v-for="specie in species" :key="specie['Scientific Name']">
-      <Card
-        :img="specie['Species Illustration Photo']['src']"
-        :name="specie['Scientific Name']"
-      />
-    </div>
+    <input v-model="state.searchText" @keyup.enter="fetchData" />
+
+    <Card
+      v-for="specie in state.species"
+      :key="specie['Scientific Name']"
+      :img="specie['Species Illustration Photo']['src']"
+      :name="specie['Species Name']"
+    >
+      <h1>test</h1>
+    </Card>
   </div>
 </template>
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, reactive, onMounted } from "vue";
 import Card from "../card/card.vue";
 import "./CardList.style.scss";
 
@@ -18,23 +22,34 @@ export default defineComponent({
   components: {
     Card,
   },
-  data() {
-    return {
+
+  setup() {
+    const state = reactive({
       species: [],
+      searchText: "",
+    });
+
+    const fetchData = async () => {
+      const response = await fetch(
+        `https://www.fishwatch.gov/api/species${
+          state.searchText &&
+          "/" + state.searchText.toLocaleLowerCase().replace(" ", "-")
+        }`
+      );
+      const data = await response.json();
+      state.species = data;
+      console.log(state.species);
+      console.log(state.searchText);
     };
-  },
-  mounted() {
-    this.fetchData();
-  },
-  methods: {
-    fetchData() {
-      fetch("https://www.fishwatch.gov/api/species")
-        .then((response) => response.json())
-        .then((data) => {
-          this.species = data;
-          console.log(this.species);
-        });
-    },
+
+    onMounted(() => {
+      fetchData();
+    });
+
+    return {
+      state,
+      fetchData,
+    };
   },
 });
 </script>
--- src/components/card/Card.vue
+++ src/components/card/Card.vue
@@ -2,6 +2,7 @@
   <div class="card">
     <div class="card__img">
       <img :src="img" alt="" />
+      <slot></slot>
     </div>
     <div class="card__content">
       <h2 class="card__title">{{ name }}</h2>
@@ -11,10 +12,13 @@
 </template>
 <script lang="ts">
 import { defineComponent } from "vue";
-import "./Card.style.scss";
 
 export default defineComponent({
   name: "Card",
   props: { img: String, name: String },
 });
 </script>
+
+<style lang="scss" scoped>
+@import "./Card.style.scss";
+</style>
